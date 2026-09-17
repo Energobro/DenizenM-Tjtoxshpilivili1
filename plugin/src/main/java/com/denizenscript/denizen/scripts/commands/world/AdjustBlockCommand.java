249c18f0e5af7f1d1ccdf29a7503e5b3a22d2a0d
Energobro <mishavmm2008@gmail.com>
2026-09-17 21:29:34 +0200

    `adjustblock` command update: now accepts MapTag like `adjust` command

--- plugin/src/main/java/com/denizenscript/denizen/scripts/commands/world/AdjustBlockCommand.java
+++ plugin/src/main/java/com/denizenscript/denizen/scripts/commands/world/AdjustBlockCommand.java
@@ -3,33 +3,36 @@ package com.denizenscript.denizen.scripts.commands.world;
 import com.denizenscript.denizen.nms.NMSHandler;
 import com.denizenscript.denizen.objects.LocationTag;
 import com.denizenscript.denizen.objects.MaterialTag;
-import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
+import com.denizenscript.denizencore.exceptions.InvalidArgumentsRuntimeException;
 import com.denizenscript.denizencore.objects.*;
-import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.core.ListTag;
+import com.denizenscript.denizencore.objects.core.MapTag;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
 import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
-import com.denizenscript.denizencore.utilities.debugging.Debug;
+import com.denizenscript.denizencore.scripts.commands.generator.*;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 
+import com.denizenscript.denizencore.utilities.text.StringHolder;
+
 import java.util.List;
+import java.util.Map;
 
 public class AdjustBlockCommand extends AbstractCommand {
 
     public AdjustBlockCommand() {
         setName("adjustblock");
-        setSyntax("adjustblock [<location>|...] [<mechanism>](:<value>) (no_physics)");
+        setSyntax("adjustblock [<location>|...] [<mechanism>](:<value>)/[<map>] (no_physics)");
         setRequiredArguments(2, 3);
         isProcedural = false;
         allowedDynamicPrefixes = true;
+        autoCompile();
     }
 
     // <--[command]
     // @Name AdjustBlock
-    // @Syntax adjustblock [<location>|...] [<mechanism>](:<value>) (no_physics)
+    // @Syntax adjustblock [<location>|...] [<mechanism>](:<value>)/[<map>] (no_physics)
     // @Required 2
     // @Maximum 3
     // @Short Adjusts a mechanism on the material of a block at the location.
@@ -41,6 +44,9 @@ public class AdjustBlockCommand extends AbstractCommand {
     // That is, an equivalent to <@link command adjust>, but that directly applies a "MaterialTag" mechanism onto a block.
     //
     // Input a location or list of locations, and the mechanism to apply.
+    //
+    // You can optionally adjust a MapTag of mechanisms to values, which are applied in the order the map lists them.
+    // Physics, if not disabled, apply once per location after the whole map, not once per mechanism.
     //
     // Use the "no_physics" argument to indicate that the change should not apply a physics update.
     // If not specified, physics will apply to the block and nearby blocks.
@@ -56,6 +62,10 @@ public class AdjustBlockCommand extends AbstractCommand {
     // Use to switch on the lever that the player is looking at, without actually providing redstone power.
     // - adjustblock <player.cursor_on> switched:true no_physics
     //
+    // @Usage
+    // Use to apply several mechanisms to one block at once.
+    // - adjustblock <player.cursor_on> <map[waterlogged=true;facing=north]>
+    //
     // -->
 
     @Override
@@ -63,55 +73,39 @@ public class AdjustBlockCommand extends AbstractCommand {
         tab.add(PropertyParser.propertiesByClass.get(MaterialTag.class).propertiesByMechanism.keySet());
     }
 
-    @Override
-    public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
-        for (Argument arg : scriptEntry) {
-            if (!scriptEntry.hasObject("locations")
-                    && arg.matchesArgumentList(LocationTag.class)) {
-                scriptEntry.addObject("locations", arg.asType(ListTag.class).filter(LocationTag.class, scriptEntry));
-            }
-            else if (!scriptEntry.hasObject("no_physics")
-                && arg.matches("no_physics")) {
-                scriptEntry.addObject("no_physics", new ElementTag(true));
-            }
-            else if (!scriptEntry.hasObject("mechanism")) {
-                if (arg.hasPrefix()) {
-                    scriptEntry.addObject("mechanism", new ElementTag(arg.getPrefix().getValue()));
-                    scriptEntry.addObject("mechanism_value", arg.object);
-                }
-                else {
-                    scriptEntry.addObject("mechanism", arg.asElement());
-                }
-            }
-            else {
-                arg.reportUnhandled();
+    public static void autoExecute(ScriptEntry scriptEntry,
+                                   @ArgName("locations") @ArgLinear @ArgSubType(LocationTag.class) List<LocationTag> locations,
+                                   @ArgName("mechanism") @ArgRaw @ArgLinear @ArgDefaultNull String mechanismRaw,
+                                   @ArgName("no_physics") boolean noPhysics) {
+        if (mechanismRaw == null) {
+            throw new InvalidArgumentsRuntimeException("You must specify a mechanism!");
+        }
+        MapTag mechanismMap = null;
+        Argument mechanismArgument = null;
+        if (mechanismRaw.startsWith("map@")) {
+            mechanismMap = MapTag.valueOf(mechanismRaw, scriptEntry.getContext());
+            if (mechanismMap == null) {
+                throw new InvalidArgumentsRuntimeException("Invalid map of mechanisms: " + mechanismRaw);
             }
         }
-        if (!scriptEntry.hasObject("locations")) {
-            throw new InvalidArgumentsException("You must specify a location!");
+        else {
+            mechanismArgument = new Argument(mechanismRaw);
         }
-        if (!scriptEntry.hasObject("mechanism")) {
-            throw new InvalidArgumentsException("You must specify a mechanism!");
-        }
-    }
-
-    @Override
-    public void execute(ScriptEntry scriptEntry) {
-        ElementTag mechanismName = scriptEntry.getElement("mechanism");
-        ObjectTag value = scriptEntry.getObjectTag("mechanism_value");
-        ElementTag noPhysics = scriptEntry.getElement("no_physics");
-        List<LocationTag> locations = (List<LocationTag>) scriptEntry.getObject("locations");
-        if (scriptEntry.dbCallShouldDebug()) {
-            Debug.report(scriptEntry, getName(), db("locations", locations), mechanismName, noPhysics, value);
-        }
-        boolean doPhysics = noPhysics == null || !noPhysics.asBoolean();
+        boolean doPhysics = !noPhysics;
         for (LocationTag location : locations) {
             Block block = location.getBlock();
             BlockData data = block.getBlockData();
             MaterialTag specialMaterial = new MaterialTag(data);
-            Mechanism mechanism = new Mechanism(mechanismName.asString(), value, scriptEntry.getContext());
-            specialMaterial.safeAdjust(mechanism);
+            if (mechanismMap != null) {
+                for (Map.Entry<StringHolder, ObjectTag> entry : mechanismMap.entrySet()) {
+                    specialMaterial.safeAdjust(new Mechanism(entry.getKey().str, entry.getValue(), scriptEntry.getContext()));
+                }
+            }
+            else {
+                boolean hasValue = mechanismArgument.hasPrefix();
+                specialMaterial.safeAdjust(new Mechanism(hasValue ? mechanismArgument.getPrefix().getValue() : mechanismArgument.getValue(),
+                        hasValue ? mechanismArgument.object : null, scriptEntry.getContext()));
+            }
             if (doPhysics) {
                 block.setBlockData(data, false);
                 applyPhysicsAt(location);
